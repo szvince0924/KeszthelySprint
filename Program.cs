e0e81a0a6b904bcf28f09f574c65d81ac00d7124
szvince0924 <163414915+szvince0924@users.noreply.github.com>
2024-12-09 14:07:24 +0100

    5.a

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace KeszthelySprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> versenyzok = new();
+
+            foreach (var sor in File.ReadAllLines("eredmenyek.txt"))
+            {
+                versenyzok.Add(new Versenyzo (sor));
+            }
+
+            Console.WriteLine($"A versenyt {versenyzok.Count}versenyzo fejezte be");
+
+
+
+
+
+            int junior = 0;
+            foreach (var v in versenyzok)
+            {
+                if (v.kategoria=="elit junior")
+                {
+                    junior++;
+                }
+
+            }
+            Console.WriteLine($"3.feladat:versenyzok szama az elit junior kategoriaba:{junior}fő");
+
+
+            //4
+            int össz_életkor = 0;
+            foreach (var v in versenyzok)
+            {
+                össz_életkor += DateTime.Now.Year - v.szuletesi_ev;
+            }
+            Console.WriteLine($"5.feladat")
+            string ketegória = Console.ReadLine
+        }
+
+
+
+        //5
+       
+        
+    }
+}
